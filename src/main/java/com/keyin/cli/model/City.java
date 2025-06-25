c83f5cd3136d7148bd38d0ea470b4af9a1c9521c
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-06-25 09:44:11 -0230

    Airport modal created

---
+++ src/main/java/com/keyin/cli/model/City.java
@@ -0,0 +1,19 @@
+package com.keyin.cli.model;
+
+public class City {
+    private String name;
+
+    public City() {}
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
